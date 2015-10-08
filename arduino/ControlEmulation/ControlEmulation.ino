fbb688a95aa6dc1a4f2bf7a7c77924e7f3ecb940
serred03 <edse.3103@gmail.com>
2015-10-08 00:03:56 -0500

    Added command parse on arduino

--- arduino/ControlEmulation/ControlEmulation.ino
+++ arduino/ControlEmulation/ControlEmulation.ino
@@ -13,6 +13,7 @@ Servo aileron_channel;
 Servo elevator_channel; 
 Servo throttle_channel; 
 Servo rudder_channel; 
+Servo aux_channel;
 
 const int _max = 130; //max on ail, ele, rudder.
 const int _min = 56; //min on ail,ele,rudder
@@ -23,8 +24,9 @@ int aileron_value = 93; //93 is the middle value.
 int elevator_value = 93; 
 int throttle_value = 59; //59 is idle and 128 is full
 int rudder_value = 93; 
-
-String to_print= "";
+int aux_value = 93;
+int _pwm_value = 0; 
+String command;
 
 void setup()
 {
@@ -32,78 +34,114 @@ void setup()
   elevator_channel.attach(5);
   throttle_channel.attach(6);
   rudder_channel.attach(9);
+  aux_channel.attach(10);
   Serial.begin(9600); 
 }
 
 void loop()
 {
-  if(Serial.available() >0) 
-  {
-
-    int inByte = Serial.read(); 
-    _inc_channel_out(inByte);
-
-  }
-
-  aileron_channel.write(aileron_value); 
-  elevator_channel.write(elevator_value); 
-  throttle_channel.write(throttle_value);
-  rudder_channel.write(rudder_value); 
+  _read_command();
 }
 
-void _inc_channel_out(int inByte)
+
+void _read_command()
 {
-  switch(inByte)
+  while(Serial.available())
   {
-    case 'a':
-      if(aileron_value < _max) aileron_value +=1;
-      Serial.print("aileron_value: ");
-      Serial.print(aileron_value);
-      Serial.println(" right");
-      break;
-    case 'z':
-      if(aileron_value > _min) aileron_value -=1;
-      Serial.print("aileron_value: ");
-      Serial.print(aileron_value);
-      Serial.println(" left");
-      break;
-    case 's':
-      if(elevator_value < _max) elevator_value +=1;
-      Serial.print("elevator_value: ");
-      Serial.print(elevator_value);
-      Serial.println(" back");
-      break;
-    case 'x':
-      if(elevator_value > _min) elevator_value -=1;
-      Serial.print("elevator_value: ");
-      Serial.print(elevator_value);
-      Serial.println(" forward"); 
-      break; 
-    case 'd':
-      if(throttle_value < _max_t) throttle_value +=1;
-      Serial.print("throttle_value: ");
-      Serial.print(throttle_value);
-      Serial.println(" +t");
-      break;
-    case 'c':
-      if(throttle_value > _min_t) throttle_value -=1;
-      Serial.print("throttle_value: ");
-      Serial.print(throttle_value);
-      Serial.println(" -t");
-      break;
-    case 'f':
-      if(rudder_value < _max) rudder_value +=1;
-      Serial.print("rudder_value: ");
-      Serial.print(rudder_value);
-      Serial.println(" left");
-      break;
-    case 'v':
-      if(rudder_value > _min) rudder_value -=1;
-      Serial.print("rudder_value: "); 
-      Serial.print(rudder_value);
-      Serial.println(" right");
-      break;
+    char c = Serial.read(); 
+    
+    if(c =='\n')
+    {
+      _parse_command(command);
+      command = "";
+    }
+    else
+    {
+      command += c;
+    }
+  }  
+}
+void _parse_command(String com)
+{
+  String part1; 
+  String part2; 
+  
+  int underscore_index = com.indexOf("_");
+  
+  if(underscore_index > 0)
+  {
+    part1 = com.substring(0,underscore_index);
+    part2 = com.substring(underscore_index + 1);
+    
+    int value = part2.toInt();
+    
+    if(!part1.equals("TH"))
+    {
+      _pwm_value = min(max(value,_min),_max); //trim value
+    }else{
+      
+      _pwm_value = min(max(value,_min_t),_max_t);//trim value to 
+                                                 //throttle vals.
+    }    
+    
+     _write_to_channel(part1,_pwm_value);
+    
+    Serial.print(part1);
+    Serial.print(": ");
+    Serial.println(_pwm_value);   
+  }
+  else
+  {
+    part1 = com;
+    Serial.println("Not doing anything");
+  }
+}
+
+void _write_to_channel(String channel, int pwm)
+{ 
+  if(channel.equals("AI"))
+  {
+    aileron_value = pwm;
+    aileron_channel.write(pwm);
+  }
+  else if(channel.equals("EL"))
+  {
+    elevator_value = pwm;
+    elevator_channel.write(pwm);
+  }
+  else if(channel.equals("TH"))
+  {
+    throttle_value = pwm;
+    throttle_channel.write(pwm);
+  }
+  else if(channel.equals("RD"))
+  {
+    rudder_value = pwm;
+    rudder_channel.write(pwm); 
+  }
+  else if(channel.equals("AX"))
+  {
+    aux_value = pwm; 
+    aux_channel.write(pwm);
+  }
+  else
+  {
+    _print_error(0); //Error 0 is incorrect command
+  }
+  
+}
+
+void _print_error(int e)
+{
+  switch(e)
+  {
+    case 0:
+          Serial.println("Incorrect command"); 
+          break; 
+    case 1:
+          Serial.println("Some");
+          break; 
     default:
-      break;
+          break;
   }
 }
