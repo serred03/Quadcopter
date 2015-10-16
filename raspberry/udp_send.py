eba47ddd2222c9b40c7eca7a9f94fd6bce66df1e
serred03 <edse.3103@gmail.com>
2015-10-16 12:15:59 -0500

    Added prompt to udp_send

--- raspberry/udp_send.py
+++ raspberry/udp_send.py
@@ -2,11 +2,15 @@ import socket
 
 UDP_IP = "192.168.1.20"
 UDP_PORT = 5005
-MESSAGE = "Hello, World!"
+commandE = ""
 
-print "UDP target IP:", UDP_IP
-print "UDP target port:", UDP_PORT
-print "message:", MESSAGE
 
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-sock.sendto(MESSAGE,(UDP_IP,UDP_PORT))
+print "Enter command to send, and 'exit' to terminate program"
+
+while 1:
+    command = raw_input(">>")
+    sock.sendto(command,(UDP_IP,UDP_PORT))
+    if command == "exit":
+        exit()
+    
