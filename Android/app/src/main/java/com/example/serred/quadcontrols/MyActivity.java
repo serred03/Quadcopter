07f9b3269719a29e74b96bdc8c99e9a5b345210a
serred03 <edse.3103@gmail.com>
2015-11-08 00:09:38 -0600

    Corrected aileron direction

--- Android/app/src/main/java/com/example/serred/quadcontrols/MyActivity.java
+++ Android/app/src/main/java/com/example/serred/quadcontrols/MyActivity.java
@@ -230,7 +230,7 @@ public class MyActivity extends ActionBarActivity implements SensorEventListener
                 mapped_value = Math.round(v * 70/100+58);
                 break;
             case 'a'://aileron
-                v *= -100;
+                v *= 100;
                 if(v < (limits*-1)) {
                     v = -80;
                 } else if (v > limits) {
@@ -239,7 +239,7 @@ public class MyActivity extends ActionBarActivity implements SensorEventListener
                 mapped_value = Math.round(((v+limits)*arduino_range)/(2*limits) + arduino_min);
                 break;
             case 'e'://elevator
-                v *= 100;
+                v *= -100;
                 if(v < (limits*-1)) {
                     v = -80;
                 } else if (v > limits) {
