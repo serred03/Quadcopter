4d2c22914e0025ad0bd3a4625662672f14d9e79c
serred03 <edse.3103@gmail.com>
2015-11-09 22:02:05 -0600

    Xbox control with UDP connection

--- XboxControl/main.cpp
+++ XboxControl/main.cpp
@@ -10,13 +10,39 @@
 #include<iostream>
 #include<arpa/inet.h>
 #include<sys/socket.h>
+#include<sys/types.h> 
+#include<unistd.h>
+#include<netinet/in.h>
+#include<netdb.h>
+#include<memory.h> 
+#include<ifaddrs.h> 
+#include<net/if.h>
+#include<errno.h> 
+#include<stdlib.h> 
+#include<sstream>
 
-#define BUFLEN 1024
-#define PORT 5005
 
 using namespace std; 
 
 int map_value(int);
+
+int resolvehelper(const char* hostname, int family, const char* service, sockaddr_storage* pAddr)
+{
+    int result;
+    addrinfo* result_list = NULL;
+    addrinfo hints = {};
+    hints.ai_family = family;
+    hints.ai_socktype = SOCK_DGRAM; // without this flag, getaddrinfo will return 3x the number of addresses (one for each socket type).
+    result = getaddrinfo(hostname, service, &hints, &result_list);
+    if (result == 0)
+    {
+        //ASSERT(result_list->ai_addrlen <= sizeof(sockaddr_in));
+        memcpy(pAddr, result_list->ai_addr, result_list->ai_addrlen);
+        freeaddrinfo(result_list);
+    }
+
+    return result;
+}
 
 int main(int argc, char* argv[])
 {
@@ -45,6 +71,32 @@ int main(int argc, char* argv[])
 	int map_elevator; 
 	int map_aileron;
 
+    	int result = 0;
+        int sock = socket(AF_INET, SOCK_DGRAM, 0);
+
+        char szIP[100];
+
+        sockaddr_in addrListen = {}; // zero-int, sin_port is 0, which picks a random port for bind.
+        addrListen.sin_family = AF_INET;
+        result = bind(sock, (sockaddr*)&addrListen, sizeof(addrListen));
+        if (result == -1)
+        {
+           int lasterror = errno;
+           std::cout << "error: " << lasterror;
+           exit(1);
+        }
+
+
+        sockaddr_storage addrDest = {};
+        result = resolvehelper("10.20.6.174", AF_INET, "5005", &addrDest);
+        if (result != 0)
+        {
+           int lasterror = errno;
+           std::cout << "error: " << lasterror;
+           exit(1);
+        }
+
+        
 	control_stream = popen("xboxdrv","r");
 	if(control_stream == NULL)
 	{
@@ -57,6 +109,8 @@ int main(int argc, char* argv[])
 	while(!feof(control_stream))
 	{
 		command = stream;
+
+		stringstream ss; 
 
 		throttle_index = command.find("Y1"); 
 		rudder_index = command.find("X1"); 
@@ -71,7 +125,7 @@ int main(int argc, char* argv[])
 
 			map_throttle = map_value(throttle_value); 
 
-			cout << "map_throttle = " << map_throttle << "\t"; 
+			ss << "TH_" << map_throttle << " ";
 
 		}
 		if(rudder_index != -1)
@@ -82,7 +136,7 @@ int main(int argc, char* argv[])
 
 			map_rudder = map_value(rudder_value); 
 
-			cout << "map_rudder = " << map_rudder << "\t "; 
+			ss << "RD_" << map_rudder<< " ";
 		}
 		if(elevator_index != -1)
 		{
@@ -92,7 +146,7 @@ int main(int argc, char* argv[])
 
 			map_elevator = map_value(elevator_value); 
 
-			cout << "map_elevator = " << map_elevator << "\t"; 
+			ss << "EL_" << map_elevator << " ";
 		}
 		if(aileron_index != -1)
 		{
@@ -102,13 +156,21 @@ int main(int argc, char* argv[])
 
 			map_aileron = map_value(aileron_value);
 
-			cout << "map_aileron = " << map_aileron << endl; 
+			ss << "AI_" << map_aileron<< "\n";
 		}
 
 		
 
 
 		fgets(stream,1000,control_stream); 		
+
+		string str = ss.str();
+
+		const char* msg = str.c_str();
+                size_t msg_length = strlen(msg);
+
+                result = sendto(sock, msg, msg_length, 0, (sockaddr*)&addrDest, sizeof(addrDest));
+
 	}
 
 	pclose(control_stream); 
